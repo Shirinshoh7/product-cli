c1b108be7836d7ac9d4dca8e769f9c05f56ebadc
shirinshosh <badalovshirinshoh29@gmail.com>
2025-12-09 16:03:54 +0600

    добавит все файлы

--- main.py
+++ main.py
@@ -12,27 +12,23 @@ def main():
     manager = ProductManager(filename)
 
     if action == "add":
-        # python main.py products.txt add "Огурцы" 50
         name = sys.argv[3]
         price = int(sys.argv[4])
         manager.add_product(name, price)
         print("Добавлено.")
 
     elif action == "edit":
-        # python main.py products.txt edit "Огурцы" 60
         name = sys.argv[3]
         new_price = int(sys.argv[4])
         manager.edit_product(name, new_price)
         print("Изменено.")
 
     elif action == "delete":
-        # python main.py products.txt delete "Огурцы"
         name = sys.argv[3]
         manager.delete_product(name)
         print("Удалено.")
 
     elif action == "sum":
-        # python main.py products.txt sum
         print("Общая сумма:", manager.total_sum())
 
     else:
